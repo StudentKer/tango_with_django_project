f3ce67134c1b9e80ebb3b4cdcdbb0fb01aa9b9e5
Ke Liu <2529840L@student.gla.ac.uk>
2021-07-27 13:57:32 +0800

    add media. 4.3 OK!

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,12 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -69,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
